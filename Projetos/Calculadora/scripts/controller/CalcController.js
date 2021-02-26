21b8bff25b8d48e41e44c9cb9d7ddb169884e79b
JOÃO LUIZ RIBEIRO GARBINI <43076430+Joaogarbini@users.noreply.github.com>
2021-02-25 22:50:41 -0300

    actual

--- Projetos/Calculadora/scripts/controller/CalcController.js
+++ Projetos/Calculadora/scripts/controller/CalcController.js
@@ -1,6 +1,8 @@
 class CalcController{
     constructor(){
-        this._displayCalc ="0";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
         this.currentDate;
         this.initialize();
 
@@ -8,24 +10,20 @@ class CalcController{
 
     initialize(){
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector('#data');
-        let timeEl = document.querySelector('#hora');
-
-        displayCalcEl.innerHTML = '4567';
-        dateEl.innerHTML = '01/05/2020';
-        timeEl.innerHTML = '00:00';
+               
+        this.dateEl.innerHTML = '01/05/2020';
+        this.timeEl.innerHTML = '00:00';
 
 
 
     }
 
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }  
 
     set displayCalc(valor){
-        this._displayCalc = valor;
+        this._displayCalcEl.innerHTML= valor;
     }  
 
     get dataAtual(){
